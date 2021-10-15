a1f5d746886966bed07906ec412d0d78d59d10fc
GreenAcademy <example@noreply.com>
2021-10-15 11:49:43 +0900

    누락 복구

---
+++ src/main/java/io/github/woukies/ex/Calculation.java
@@ -0,0 +1,41 @@
+package io.github.woukies.ex;
+
+public class Calculation {
+	private int firstNum, secondNum;
+
+	public void add() {
+		System.out.println("add");
+		System.out.println(firstNum + "+" + secondNum + "=" + (firstNum + secondNum));
+	}
+
+	public void sub() {
+		System.out.println("sub");
+		System.out.println(firstNum + "-" + secondNum + "=" + (firstNum - secondNum));
+	}
+
+	public void mult() {
+		System.out.println("mult");
+		System.out.println(firstNum + "*" + secondNum + "=" + (firstNum * secondNum));
+	}
+
+	public void div() {
+		System.out.println("div");
+		System.out.println(firstNum + "/" + secondNum + "=" + (firstNum / secondNum));
+	}
+
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+}
