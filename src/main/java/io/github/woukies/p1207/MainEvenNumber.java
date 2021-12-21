e9378d5ae0a4f8a5157893c30ae8353ddef32590
Github <github@noreply.com>
2021-12-21 10:44:30 +0900

    fix p1207

--- src/main/java/io/github/woukies/p1207/MainEvenNumber.java
+++ src/main/java/io/github/woukies/p1207/MainEvenNumber.java
@@ -5,7 +5,8 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainEvenNumber {
 	public static void main(String[] args) {
-		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:/p1207/evenNumberCTX.xml");
+		AbstractApplicationContext ctx = 
+				new GenericXmlApplicationContext("classpath:/p1207/evenNumberCTX.xml");
 		MyEvenNumber myEvenNumber = ctx.getBean(MyEvenNumber.class);
 		myEvenNumber.pc();
 		ctx.close();
